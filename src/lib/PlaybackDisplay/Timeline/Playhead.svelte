d75892efd0938cf9c24ceb4d24413102e92a8f79
Webb Hinton <38958118+wyhinton@users.noreply.github.com>
2026-01-30 13:36:27 -0800

    SAM-82 Fix issue with sample cache playback

--- src/lib/PlaybackDisplay/Timeline/Playhead.svelte
+++ src/lib/PlaybackDisplay/Timeline/Playhead.svelte
@@ -7,7 +7,10 @@
   export let currentTransform: d3.ZoomTransform;
   export let contentScaleY: number;
 
-  $: playheadCol = $appState.playingCombined ? '#68d391' : 'white';
+  let yOff = -4;
+  let playHeadWidth = 6;
+  let playHeadHeight = 6;
+  $: playheadCol = '#8dff2b';
 </script>
 
 <!-- CURRENT PLAYHEAD -->
@@ -15,19 +18,12 @@
   <!-- Vertical line -->
   <line
     x1={playHeadX}
-    y1={0}
+    y1={yOff}
     x2={playHeadX}
     y2={80}
     stroke={playheadCol}
     stroke-width={1.5 / currentTransform.k}
   />
   <!-- Arrow head at top -->
-  <path
-    d={`M ${playHeadX - 6 / currentTransform.k} 0 
-        L ${playHeadX} 12  
-        L ${playHeadX + 6 / currentTransform.k} 0 
-        Z`}
-    transform={`scale(1, ${1 / contentScaleY})`}
-    fill={playheadCol}
-  />
+  -->
 </g>
